5cbda8d0a118f1dc968dad740dfc285ff9e16e04
manik <maniksarker652@gmail.com>
2025-02-03 21:31:40 +0600

    setup for learn algorithm

---
+++ breadth-first-search/bsf.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int>adj_list[n];
+
+    while (e--)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
