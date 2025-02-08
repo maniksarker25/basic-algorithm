876cb805dc0dbe6bcfe86086ef0de1ce4215c2bc
manik <maniksarker652@gmail.com>
2025-02-09 01:40:37 +0600

    successfully bsf on 2d grid

---
+++ depth-first-search/bfs_on_2d_grid.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+// declare grid in global;
+char grid[105][105];
+bool vis[105][105];
+// declare 4 move
+vector<pair<int,int>>d = {{-1,0},{1,0},{0,-1},{0,1}};
+int n,m;
+
+
+// check valid move
+bool valid(int i,int j){
+    if(i<0 || i >= n || j < 0 || j>=m){
+        return false;
+
+    }
+    return true;
+
+
+}
+
+void bfs(int si,int sj){
+    queue<pair<int,int>>q;
+    q.push({si,sj});
+    vis[si][sj] = true;
+    while (!q.empty()){
+        pair<int,int> parent = q.front();
+        q.pop();
+        int par_i = parent.first;
+        int par_j = parent.second;
+
+        cout << par_i << ' ' << par_j << endl;
+
+        for(int i = 0;i<4;i++){
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+            if(valid(ci,cj) && !vis[ci][cj]){
+                q.push({ci,cj});
+                vis[ci][cj]= true;
+            }
+        }
+    }
+    
+
+}
+
+int main()
+{
+
+    cin >> n>>m;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+           cin >> grid[i][j];
+        }
+    }
+
+    // input src row and src column
+    int si,sj;
+    cin >> si >> sj;
+    memset(vis,false,sizeof(vis));
+    bfs(si,sj);
+    return 0;
+}   
