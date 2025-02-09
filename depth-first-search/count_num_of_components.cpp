6a35147db1bf6c43227c1376deedfa4d78dd2f5a
manik <maniksarker652@gmail.com>
2025-02-09 13:38:22 +0600

    find number of componet in graph using dfs

---
+++ depth-first-search/count_num_of_components.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>adj_list[1005];
+bool vis[1005];
+
+
+void dfs(int src){
+    // cout << src << " ";
+
+    vis[src] = true;
+    for(int child : adj_list[src]){
+        if(!vis[child]){
+            dfs(child);
+        }
+    }
+}
+
+int main()
+{
+    int n,e;
+    cin >> n>>e;
+    while (e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    int component_count = 0;
+    for(int i =0;i<n;i++){
+        if(vis[i] == false){
+            dfs(i);
+           component_count++;
+        }
+    }
+
+    cout << component_count << endl;
+    return 0;
+}
