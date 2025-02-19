ed89f1170d6c076b8be90a4ea680feebcb674e8d
manik <maniksarker652@gmail.com>
2025-02-19 19:42:40 +0600

    make changes

--- dijkstra/adj_list_for_weighted_graph.cpp
+++ dijkstra/adj_list_for_weighted_graph.cpp
@@ -2,24 +2,26 @@
 using namespace std;
 int main()
 {
-    int n,e;
-    cin >> n >>e;
-    vector<pair<int,int>>adj_list[n];
+    int n, e;
+    cin >> n >> e;
+    vector<pair<int, int>> adj_list[n];
     while (e--)
     {
-        int a,b,c;
-        cin >> a >>b >>c;
-                    adj_list[a].push_back({b,c});
-        adj_list[b].push_back({a,c});
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_list[a].push_back({b, c});
+        adj_list[b].push_back({a, c});
     }
 
-    for(int i = 0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cout << i << "=>";
-        for(pair<int,int>p : adj_list[i] ){
-            cout << p.first << ' ' << p.second << ", " ;
+        for (pair<int, int> p : adj_list[i])
+        {
+            cout << p.first << ' ' << p.second << ", ";
         }
         cout << endl;
     }
-    
+
     return 0;
 }
