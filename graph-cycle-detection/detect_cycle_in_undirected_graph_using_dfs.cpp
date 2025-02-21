22cd62c6eb2499590182eb17d4b9d9dbf87268b5
manik <maniksarker652@gmail.com>
2025-02-21 19:43:07 +0600

    practice cycle detection

--- graph-cycle-detection/detect_cycle_in_undirected_graph_using_dfs.cpp
+++ graph-cycle-detection/detect_cycle_in_undirected_graph_using_dfs.cpp
@@ -2,50 +2,57 @@
 using namespace std;
 
 bool vis[100];
-vector<int>adj_list[105];
+vector<int> adj_list[105];
 int parent_array[105];
 bool isCycle;
 
-
-
-void dfs(int src){
-   vis[src] = true;
-   for(int child : adj_list[src]){
-    if(vis[child] && parent_array[src] != child){
-        isCycle = true;
+// dffs
+void dfs(int src)
+{
+    vis[src] = true;
+    for (int child : adj_list[src])
+    {
+        if (vis[child] && parent_array[src] != child)
+        {
+            isCycle = true;
+        }
+        if (!vis[child])
+        {
+            parent_array[child] = src;
+            dfs(child);
+        }
     }
-    if(!vis[child]){
-        parent_array[child] = src;
-        dfs(child);
-    }
-   }
 }
 
 int main()
 {
-    int n ,e;
+    int n, e;
     cin >> n >> e;
     while (e--)
     {
-       int a , b;
-       cin >> a >> b;
-       adj_list[a].push_back(b);
-       adj_list[b].push_back(a);
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
     }
-    memset(vis,false,sizeof(vis));
-    memset(parent_array,-1,sizeof(parent_array));
+    memset(vis, false, sizeof(vis));
+    memset(parent_array, -1, sizeof(parent_array));
     isCycle = false;
-    for(int i = 0;i<n;i++){
-        if(!vis[i]){
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
             dfs(i);
         }
     }
-    if(isCycle){
+    if (isCycle)
+    {
         cout << "Cycle detected" << endl;
     }
-    else{
+    else
+    {
         cout << "No Cycle" << endl;
     }
-    
+
     return 0;
 }
