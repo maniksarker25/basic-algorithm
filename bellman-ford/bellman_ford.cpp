2b963f174c68a209e207b24a04ee54d8a4493ea0
manik <maniksarker652@gmail.com>
2025-02-22 22:45:23 +0600

    do bellman_ford in seperatre function

--- bellman-ford/bellman_ford.cpp
+++ bellman-ford/bellman_ford.cpp
@@ -12,26 +12,11 @@ public:
         this->c = c;
     }
 };
-
+int n, e;
 int dis[1005];
-
-int main()
+vector<Edge> edge_list;
+void bellman_ford()
 {
-    int n, e;
-    cin >> n >> e;
-    vector<Edge> edge_list;
-    while (e--)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        edge_list.push_back(Edge(a, b, c));
-    }
-    for (int i = 0; i < n; i++)
-    {
-        dis[i] = INT_MAX;
-    }
-    dis[0] = 0;
-
     for (int i = 0; i < n - 1; i++)
     {
         for (auto ed : edge_list)
@@ -46,7 +31,26 @@ int main()
             }
         }
     }
+}
 
+int main()
+{
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a, b, c));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
+
+    // call bellman_ford
+    bellman_ford();
     for (int i = 0; i < n; i++)
     {
         cout << i << "=>" << dis[i] << endl;
