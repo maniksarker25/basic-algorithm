05b564e4d3efbbee16db02979d90f2263bf1b260
manik <maniksarker652@gmail.com>
2025-02-22 22:42:09 +0600

    implement bellman ford algorithmn

--- bellman-ford/bellman_ford.cpp
+++ bellman-ford/bellman_ford.cpp
@@ -13,6 +13,8 @@ public:
     }
 };
 
+int dis[1005];
+
 int main()
 {
     int n, e;
@@ -24,6 +26,30 @@ int main()
         cin >> a >> b >> c;
         edge_list.push_back(Edge(a, b, c));
     }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
 
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
+            int a, b, c;
+            a = ed.a;
+            b = ed.b;
+            c = ed.c;
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            {
+                dis[b] = dis[a] + c;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << "=>" << dis[i] << endl;
+    }
     return 0;
 }
