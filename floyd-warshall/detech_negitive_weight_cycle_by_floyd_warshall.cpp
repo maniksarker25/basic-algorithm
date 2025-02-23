60196ea65b812838796b5f4a9b354001a57e87c7
manik <maniksarker652@gmail.com>
2025-02-23 21:54:38 +0600

    negitive weight cycle detech with floyd-warshall algo

---
+++ floyd-warshall/detech_negitive_weight_cycle_by_floyd_warshall.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_mat[i][j] = 0;
+            }
+            else
+            {
+
+                adj_mat[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_mat[a][b] = c;
+        // adj_mat[b][a] = c; // if graph is undirected
+    }
+
+    // floyd warshall
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (adj_mat[i][k] != INT_MAX && adj_mat[k][j] != INT_MAX && adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j])
+                {
+                    adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
+                }
+            }
+        }
+    }
+
+    // detech negitive weigthed cycle
+    bool cycle = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (adj_mat[i][i] < 0)
+        {
+            cycle = true;
+        }
+    }
+    if (cycle)
+    {
+        cout << "Negitive weighted cycle detechted" << endl;
+    }
+    else
+    {
+        // print
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << adj_mat[i][j] << ' ';
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
