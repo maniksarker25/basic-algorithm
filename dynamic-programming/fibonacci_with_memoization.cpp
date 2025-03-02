816cf152c46615c2ecf473d2796792230ec31d53
manik <maniksarker652@gmail.com>
2025-03-02 23:07:25 +0600

    fibonacci with memoization for optimized with dp

--- dynamic-programming/fibonacci_with_memoization.cpp
+++ dynamic-programming/fibonacci_with_memoization.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
-using namespace std; // O(2 the power n)
-
+using namespace std; // O(n)
+int dp[1005];
 
 int fibo(int n)
 {
    if(n == 0 || n==1){
     return n;
    }
-    return fibo(n-1) + fibo(n-2);
+    dp[n] = fibo(n-1) + fibo(n-2);
+    return dp[n];
 }
 
 int main(){
+    memset(dp,-1,sizeof(dp));
     cout << fibo(9) << endl;
     return 0;
 }
