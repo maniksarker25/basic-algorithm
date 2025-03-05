3baefcc57111fff086644129eb0aeb7da2c4e3eb
manik <maniksarker652@gmail.com>
2025-03-05 21:28:00 +0600

    solve the maximum path sum

--- practice-dp/the_maxium_path_sum.cpp
+++ practice-dp/the_maxium_path_sum.cpp
@@ -1,32 +1,33 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, m;
-int a[11][11], dp[11][11];
+long long a[11][11], dp[11][11];
 
-int maximum_sum(int i, int j)
+long long maximum_sum(int i, int j)
 {
+    if (dp[i][j] != -1)
+    {
+        return dp[i][j];
+    }
     if (i == n && j == m)
     {
         dp[i][j] = a[i][j];
         return dp[i][j];
     }
 
-    if (dp[i][j] != -1)
-    {
-        return dp[i][j];
-    }
-
-    int right = 0, down = 0;
-
-    if (j < m)
-        right = maximum_sum(i, j + 1);
-
+    long long right = LLONG_MIN, down = LLONG_MIN;
     if (i < n)
+    {
         down = maximum_sum(i + 1, j);
-
-    dp[i][j] = a[i][j] + max(right, down);
-
+    }
+    if (j < m)
+    {
+        right = maximum_sum(i, j + 1);
+    }
+    dp[i][j] = max(right, down) + a[i][j];
     return dp[i][j];
 }
 
@@ -40,7 +41,6 @@ int main()
             dp[i][j] = -1;
         }
     }
-
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
@@ -49,8 +49,7 @@ int main()
         }
     }
 
-    int ans = maximum_sum(1, 1);
-
+    long long ans = maximum_sum(1, 1);
     cout << ans << endl;
 
     return 0;
