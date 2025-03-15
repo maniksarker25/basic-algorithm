484df405dccc04c327e386decb5ff8a97704d3b2
manik <maniksarker652@gmail.com>
2025-03-15 08:32:03 +0600

    s

--- final-contest/problem-1.cpp
+++ final-contest/problem-1.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << "nice";
+    int n, q;
+    cin >> n >> q;
+    
+    vector<long long> a(n + 1, 0); 
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    vector<long long> prefix_arr(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        prefix_arr[i] = prefix_arr[i - 1] + a[i]; 
+    }
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << (prefix_arr[r] - prefix_arr[l - 1]) << endl;
+    }
+
     return 0;
-}   
+}
--- final-contest/problem-6.cpp
+++ final-contest/problem-6.cpp
@@ -1,11 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main()
-{
+long long countSubsequences(const string& s, const string& t) {
+    int n = s.length();
+    int m = t.length();
+    
+    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+    
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 1;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i-1] == t[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+    return dp[n][m];
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << "nice";
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        string t;
+        cin >>t;
+        
+        long long result = countSubsequences(s, t);
+        cout << result << endl;
+    }
+    
     return 0;
-}   
+}
